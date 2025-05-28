257e95498bd2a0db2f8fd543241d3b373889e5db
Robin Schildermans <robin.schildermans@vub.be>
2025-05-29 01:25:18 +0200

    final

--- 2_DistanceMatrix.py
+++ 2_DistanceMatrix.py
@@ -3,8 +3,9 @@ import openrouteservice
 import numpy as np
 import time
 
-# openrouteservice API-sleutel
-API_KEY = "5b3ce3597851110001cf6248bd3198958c8a406bb2e2fe4994b20d50"
+# openrouteservice API-sleutel (VUL ZELF AAN)
+API_KEY = "..."
+# VOORBEELD: API_KEY = "5b3ce397851110001cf6248bd398958c8a4e06beb2e2f49194b20d50"
 client = openrouteservice.Client(key=API_KEY)
 
 # Lees coördinaten
